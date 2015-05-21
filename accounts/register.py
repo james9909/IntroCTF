48ee792de1b3d4972a70964249561840a12e1d31
James Wang <jameswang9909@hotmail.com>
2015-05-21 18:09:07 -0400

    Fix register.py

--- accounts/register.py
+++ accounts/register.py
@@ -6,12 +6,12 @@ print """
 <html>
     <head>
         <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <link type="../text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
 
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 
-        <title>IntroCTF - Login</title>
+        <title>IntroCTF - Register</title>
     </head>
 
     <body>
@@ -64,24 +64,29 @@ if "team" not in inputs or "pass" not in inputs or "confirm_pass" not in inputs:
     status = 4
 
 if status != 4:
-    if not inputs["team"].value.isalnum() or not inputs["pass"].valie.isalnum():
+    if not inputs["team"].value.isalnum() or not inputs["pass"].value.isalnum():
         status = 3
 
 if status < 3:
     newTeam = inputs["team"].value
     newPassword = inputs["pass"].value
-    confirmNewPass = inputs["confirm_pass"]
+    confirmNewPass = inputs["confirm_pass"].value
 
-    user_info = open("user_info.txt", "r+w").read()
-    user_list1 = user_info.split("\n")
+    user_info = open("user_info.txt", "r+w")
+    read_info = user_info.read()
+    user_list1 = read_info.split("\n")
     user_list2 = []
     user_dict = {}
 
-    # Separate team name and password
+    # Separate team names and password
     for info in user_list1:
         user_list2.append(info.split(","))
 
-    # Link team name and password
+    # Remove empty strings that will throw errors later on
+    while [""] in user_list2:
+        user_list2.remove([""])
+
+    # Link team names and password
     for info in user_list2:
         user_dict[info[0]] = info[1]
 
@@ -93,19 +98,23 @@ if status < 3:
 
 if status == 0:
     # Hash password for higher security
-    hashed.update(NewPassword)
+    hashed.update(newPassword)
     newPassword = hashed.hexdigest()
     new_user = "\n%s,%s" %(newTeam, newPassword)
     user_info.write(new_user)
     print "Thank you for registering! </br><a href='../index.html'>Return Home</a>"
+
 elif status == 1:
-    print "Team name is taken, <a href='signUp.html'>try Again</a>"
+    print "Team name is taken, <a href='register.html'>try again</a>"
+
 elif status == 2:
-    print "Passwords do not match, <a href='signUp.html'>try Again</a>"
+    print "Passwords do not match, <a href='register.html'>try again</a>"
+
 elif status == 3:
-    print "Please user alphanumeric characters, <a href='signUp.html'>try Again</a>"
+    print "Please user alphanumeric characters, <a href='register.html'>try again</a>"
+
 elif status == 4:
-    print "Something is missing ...<a href='signUp.html'>try Again</a>"
+    print "Something is missing ...<a href='register.html'>try again</a>"
 
 print "</body>"
 print "</html>"

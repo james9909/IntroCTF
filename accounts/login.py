7699eb3bd5f78abdce4ed5f2aab6888381b8172d
James Wang <jameswang9909@hotmail.com>
2015-05-21 18:35:50 -0400

    Begin login system

---
+++ accounts/login.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+
+print "Content-Type: text/html\n"
+print """
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--Import materialize.css-->
+        <link type="../text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+
+        <title>IntroCTF - Login</title>
+    </head>
+
+    <body>
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+
+        <nav role="navigation" style="background-color: #009688">
+            <div class="nav-wrapper container">
+                <a href="#" class="brand-logo">IntroCTF</a>
+                <ul class="right hide-on-med-and-down">
+
+                    <li><a href="../">Home</a></li>
+                    <li><a href="../scoreboard">Scoreboard</a></li>
+                    <li><a href="../problems">Problems</a></li>
+                    <li><a href="../about">About</a></li>
+                    <li><a href="./login.html">Login</a></li>
+                    <li><a href="./register.html">Register</a></li>
+
+                    <!-- <li><a class="mdi-action-info-outline modal-trigger" href="#helpmodal"></a></li> -->
+                </ul>
+                <ul id="nav-mobile" class="side-nav">
+
+                    <li><a class="waves-effect waves-indigo" href="../">Home</a></li>
+                    <li><a class="waves-effect waves-indigo" href="../scoreboard">Scoreboard</a></li>
+                    <li><a class="waves-effect waves-indigo" href="../problems">Problems</a></li>
+                    <li><a class="waves-effect waves-indigo" href="../about">About</a></li>
+                    <li><a class="waves-effect waves-indigo" href="./login.html">Login</a></li>
+                    <li><a class="waves-effect waves-indigo" href="./register.html">Register</a></li>
+
+                    <!-- <li><a class="waves-effect waves-indigo modal-trigger" href="#helpmodal"><i class="mdi-action-info-outline"></i></a></li> -->
+                </ul>
+                <a class="button-collapse" href="#" data-activates="nav-mobile"><i class="mdi-navigation-menu"></i></a>
+            </div>
+        </nav>
+    </body>
+</html>
+"""
+
+import cgi, hashlib, os, cgitb
+from time import strftime, localtime, time
+cgitb.enable()
+
+hashed = hashlib.sha1()
+status = 0
+
+inputs = cgi.FieldStorage()
+
+if "team" not in inputs or "pass" not in inputs:
+    status = 1
+
+if status != 1:
+    team = inputs["team"].value
+    password = inputs["pass"].value
+
+    user_info = open("user_info.txt", "r+w")
+    read_info = user_info.read()
+    user_list1 = read_info.split("\n")
+    user_list2 = []
+    user_dict = {}
+
+    # Separate team names and password
+    for info in user_list1:
+        user_list2.append(info.split(","))
+
+    # Remove empty strings that will throw errors later on
+    while [""] in user_list2:
+        user_list2.remove([""])
+
+    # Link team names and password
+    for info in user_list2:
+        user_dict[info[0]] = info[1]
+        user_dict['logged'] = info[2] # Prevent people from doing bad things
+
+    hashed.update(password)
+    password.hexdigest()
+
+    if password != user_dict[team]:
+        status = 1
+
+if status == 0:
+    add_info = "\n%s,%s,yes" %(team, password)
+    user_info.write(add_info)
+    print "Set-Cookie: team=%s" %(team)
+    print "<meta http-equiv='refresh'>"
+
+elif status == 1:
+    print "Invalid credentials, please <a href='login.html'>try again</a>"
+
+print "</body>"
+print "</html>"

02c5561744a9f97264fbac2c9c2b6d6a44502075
James Wang <jameswang9909@hotmail.com>
2015-05-21 23:50:41 -0400

    Update problem gen files

--- problem-static/problem-init.sh
+++ problem-static/problem-init.sh
@@ -5,6 +5,5 @@ then
     PROBLEM_NAME=$1-INCOMPLETE
 
     mkdir -p $PROBLEM_NAME/{release,admin}
-    touch $PROBLEM_NAME/solution.txt
-    cp sample/sample-validator.py $PROBLEM_NAME/validator.py
+    touch $PROBLEM_NAME/Solution.txt
 fi

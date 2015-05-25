61252ccca7ee0062f0dc10f3cd0527d7b4d1ab5b
James Wang <jameswang9909@hotmail.com>
2015-05-25 19:43:49 -0400

    Add scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
+import os
+print "Content-Type: text/html\n"
+print ""
+
 def gen_scoreboard(team_data):
     if len(team_data) == 0:
         print "There are no teams!"
     else:
-        print "<tr><th>Team</th><th>Points</th></tr>"
+        print "<br>"
+        print "<div class='container'>"
+        print "<table class='responsive-table bordered hoverable centered'>"
+        print "<thead>"
+        print "<tr><th>Rank</th><th>Team</th><th>Score</th></tr>"
+        print "</thead>"
         length = len(team_data)
         for x in range(length):
             if len(team_data) == 0:
@@ -12,7 +21,7 @@ def gen_scoreboard(team_data):
             highest_score = max(team_data.values())
             for team in team_data:
                 if team_data[team] == highest_score:
-                    print "<tr><td>%s</td><td>%s</td></tr>\n" %(team, highest_score)
+                    print "<tr><td>%d</td><td>%s</td><td>%s</td></tr>\n" %(x+1, team, highest_score)
                     del team_data[team]
                     break
 
@@ -26,6 +35,10 @@ def main():
         teams[info[0]] = info[1]
     gen_scoreboard(teams)
 
-html = open("templates/logged_out.html").read()
+if 'HTTP_COOKIE' not in os.environ:
+    html = open("templates/scoreboard_logged_out.html").read()
+else:
+    html = open("templates/scoreboard_logged_in.html").read()
+
 print html
 main()

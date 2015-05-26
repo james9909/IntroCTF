ecdab7a1abb2cb33329ac4cdee2408e3df7dc186
James Wang <jameswang9909@hotmail.com>
2015-05-26 15:02:12 -0400

    Fix error in grader

--- scripts/grader.py
+++ scripts/grader.py
@@ -7,7 +7,7 @@ def incorrect(message):
 def grade(pid, flag):
     pid = str(pid)
     if pid == "9001":
-        if flag = "introductions_are_cool":
+        if flag == "introductions_are_cool":
             return correct(5, "Welcome to IntroCTF!")
         else:
             return incorrect("Come on, try again!")

5720c6ee3d1eb7a75a728ef10c7fab08c5408c4f
James Wang <jameswang9909@hotmail.com>
2015-05-29 01:21:12 +0000

    Help me fill cookies with random values

---
+++ problem-static/Cookie-Jar_40-INCOMPLETE/admin/cookies.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<?php
+
+setcookie(test, ayyylmao, time() + (86400 * 90001), "/");
+setcookie(flag, this_cookie, time() + (86400 * 90001), "/");
+?>
+<html>
+<body>
+
+</body>
+</html>

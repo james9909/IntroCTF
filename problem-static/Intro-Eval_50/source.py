31167327cafb0257841b54917b8fffea89c2274a
James Wang <jameswang9909@hotmail.com>
2015-05-29 22:35:18 -0400

    Eval instead of exec :p

--- problem-static/Intro-Eval_50/source.py
+++ problem-static/Intro-Eval_50/source.py
@@ -1,19 +1,18 @@
-
-#!/usr/bin/python
+#!/usr/bin/python2.7
 
 del __builtins__.__dict__['__import__']
 del __builtins__.__dict__['reload']
 
-flag = "XXXXXXXXXXXXXXXXXXX"
+flag = "XXXXXXXXXXXXX"
 
 def main():
     print "Hi, welcome to the flag database. We are under construction right now, so you cannot view the flags, or do anything."
     while True:
-        command = input("What would you like to do? ")
+        command = raw_input("What would you like to do? ")
         try:
-            print "Executing command: %s" %(command)
-            exec(command)
+            result = eval(command)
+            print "Here is the result of your command: %s" %(result)
         except:
-            print "Invalid command"
+            print "Invalid command, try again"
 
 main()

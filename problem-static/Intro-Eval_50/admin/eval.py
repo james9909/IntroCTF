e99a4aa3fab84e112f5d82eafe9012f7e2be9447
James Wang <jameswang9909@hotmail.com>
2015-05-30 03:35:39 +0000

    Fix Intro Eval with Unbuffered Streams

--- problem-static/Intro-Eval_50/admin/eval.py
+++ problem-static/Intro-Eval_50/admin/eval.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python2.7
 
+import sys
+
 del __builtins__.__dict__['__import__']
 del __builtins__.__dict__['reload']
 
 flag = "eval_is_fun"
 
+class UnbufferedStream(object):
+    def __init__(self, stream):
+        self.stream = stream
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
+
+sys.stdout = UnbufferedStream(sys.stdout)
 def main():
-    print "Hi, welcome to the flag database. We are under construction right now, so you cannot view the flags, or do anything."
     while True:
-        command = raw_input("What would you like to do? ")
+        print "Welcome to the flag database! We are currently under construction. Please do not hack the flags."
         try:
-            result = eval(command)
-            print "Here is the result of your command: %s" %(result)
-        except:
-            print "Invalid command, try again"
+            command = str(raw_input("What would you like to do? "))
+            result = str(eval(command))
+            print "This is the result: %s" %(result)
+        except Exception, e:
+            print "Invalid command!!!! EXITING!!!!!"
+            return
 
 main()

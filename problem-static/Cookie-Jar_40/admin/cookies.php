b9c2525aee9b3e49e9e477f56322f60e32b50c60
James Wang <jameswang9909@hotmail.com>
2015-05-29 20:28:43 -0400

    Finish Cookie Jar

---
+++ problem-static/Cookie-Jar_40/admin/cookies.php
@@ -0,0 +1,62 @@
+<?php
+setcookie(xvlqASsvkvtBjYzfmTQRJxoLVF, sRFEAZqGYUgZPiiXAfXbNqStFh, time() + (86400 * 90001), '/');
+setcookie(jgWeSeRsLcEoNTwLztgEXCqQIC, ZcQYUoPeaFhqcGBREfnXMpxNZY, time() + (86400 * 90001), '/');
+setcookie(IUgMajVfvDCPjUqGlLsIPVoguw, QpwfbYMNgXtgYDXLxJilZDqXAv, time() + (86400 * 90001), '/');
+setcookie(kGLKtWvBMJIISyZCObjLXoZgvM, aiGUsMinwwRBhhvKMOnUmqRgvL, time() + (86400 * 90001), '/');
+setcookie(gIoxXCziSDdIrQcwUWrrraUZZb, ExlmmcUyiMRYfHzLMGiWlKZtKS, time() + (86400 * 90001), '/');
+setcookie(ywjrKTbagnbwxycoItPzBAmhyZ, zxqsBTFxujQlnQnocNPQhVLENJ, time() + (86400 * 90001), '/');
+setcookie(JRMQORkGlyFhhmsgAOCAvuqLzz, iSwkYsVTYtQwhSovLFIUrFYUio, time() + (86400 * 90001), '/');
+setcookie(aNRxzaGaopuNwlQdeBSkShKKkg, ElXquPWDkRwffjlnFpvIyFsflt, time() + (86400 * 90001), '/');
+setcookie(MuhzWyJqCJxywyEIjtysHATOgE, nwBgcWFePGrVsnXtBgvowOliSN, time() + (86400 * 90001), '/');
+setcookie(OPLiHrrvGMUITeIgKouCQGLgHj, MxqUrBBtPHvHNdlrWCuBbbXwEJ, time() + (86400 * 90001), '/');
+setcookie(pyBrognwCtENqEbQaSIEqtkbLz, aMNBKnoHBfneyDuhEXcebhfCLg, time() + (86400 * 90001), '/');
+setcookie(ujZynsoLmcdJrkNqwTRCfknGYk, mWthRlvWHQBxKDLQKUbkBYHbpm, time() + (86400 * 90001), '/');
+setcookie(FNybgRLOUvMMKSOVIxiPwbzMFx, nbVONUTDspqbkSwdoRefGabIbJ, time() + (86400 * 90001), '/');
+setcookie(BSCnsBMunXCXKULUaokVOpkrgr, hXrKYSlsPnPqVYSXMAJOrPFwGm, time() + (86400 * 90001), '/');
+setcookie(brFCogkAzjTuSVEzpCBuJKYiZt, wUboKKEogBAuOPzEysZNqobtVu, time() + (86400 * 90001), '/');
+setcookie(UYwGHDHlREoAWKeocamYlPMQiz, gvgtQRmnuPGHRKJOarEeIddKXW, time() + (86400 * 90001), '/');
+setcookie(vRhGRaRhmGuggwdnbnkhbBXPWm, QMmVxYzUcbOhKLXumiMpCSSdTg, time() + (86400 * 90001), '/');
+setcookie(AYnDXHSPgKoyvgDJduuKjstgTY, hEngrHPkVJAiFQGMXxsgtasUKU, time() + (86400 * 90001), '/');
+setcookie(gQDxmpNCDJvHlUyENCVxSGmoKK, PdEryFyvhOQwpXCeHzOgtasxzV, time() + (86400 * 90001), '/');
+setcookie(LYfoVKFAuvlRwyICVwIDaGBaIO, rLwrLdMoxizNLPhqyffvRIRAMj, time() + (86400 * 90001), '/');
+setcookie(IgKuQUbnkUGaukvIBafskRAKSk, PKlWiiwCxhPDRPZVvlaaMFzaZa, time() + (86400 * 90001), '/');
+setcookie(kkPGDyBnnmeUQRDXqukVBbLjWw, duVKfXQOicEcHmJHaYWhWlHgQK, time() + (86400 * 90001), '/');
+setcookie(OroeEAWGZtabNkfhWELMuTxGgt, UONJNMMZAwfnXGLOACAhmuKERe, time() + (86400 * 90001), '/');
+setcookie(MKdnBFzIbpIhNPJBcmfYDsefeK, byAazRyIdAheYtQvyuJimNIeqh, time() + (86400 * 90001), '/');
+setcookie(HjsHSuUGUQGGQnmbYZXgmqunEy, BhTtEsHiMoUXBdMFQEPWBoFxJx, time() + (86400 * 90001), '/');
+setcookie(LavYBQiFZEPqhonRCcKLinOUux, JZHgjPPECbrVCNfHNDYApjimjS, time() + (86400 * 90001), '/');
+setcookie(ffAfoytKlBKYIUQTlBsjdOkdlg, CXaHNmksepEIhtzaEvUYpAkARv, time() + (86400 * 90001), '/');
+setcookie(ygQBPmmeBcURFdAhIurWqhnBZz, IWBefbawWgUJYWjGtYrpleQDHO, time() + (86400 * 90001), '/');
+setcookie(kvFmvYJVPWWbjIqxIQLEhsAMDl, cGGyZRyFJmYkVxZgPbeHBnmorf, time() + (86400 * 90001), '/');
+setcookie(SRpXnUaovLCaVTPDWyQUsDLpiq, iHnXnREPVPkWbgGpHoySaxCzEc, time() + (86400 * 90001), '/');
+setcookie(ySliaPUSgVWvlpeBklNMqiPysV, nKEtGYkkuXcpisvBztIfvtKZKD, time() + (86400 * 90001), '/');
+setcookie(sEYAnqRerFmWzDhksfDOaYDHHw, xEWlVQZczGKQsHXFAqBqVZcUhx, time() + (86400 * 90001), '/');
+setcookie(QiRlMQLRUqiuoPTeHlWSTLEmAO, yGeuVnICYoQgxAvUjzBXlEyYSd, time() + (86400 * 90001), '/');
+setcookie(NKnGDyxmVbrNfgbVhgVpNFdyKi, iDjCnzkNtjfcfVONeibUNtIFHG, time() + (86400 * 90001), '/');
+setcookie(ipaEzgTBbURWxAVAYuoBhySLLf, ABaqwJaZZgrNwxYREzwGnscscP, time() + (86400 * 90001), '/');
+setcookie(DmXzaNHTABRMEXEMnMhvxyXqTL, BIUvnAYdPxCilRMDRnPDFRysEB, time() + (86400 * 90001), '/');
+setcookie(OtIeuMgYGxSHlREmLWJXjwNEUI, vfkepJmhziPdrIJzbXidcgdaXP, time() + (86400 * 90001), '/');
+setcookie(flag, coookkkkiiiieeeee, time() + (86400 * 90001), '/');
+setcookie(DjAhQOmRTmrFbKibnHOrYBPiFG, vpiNfiEmeTMGNcFGqBHpEtcVSx, time() + (86400 * 90001), '/');
+setcookie(mdqjHfIvUOPPFJyhzwUNUQxzwE, dflyXUuWuIhaZwPSpXiHyjBiJN, time() + (86400 * 90001), '/');
+setcookie(zEVkmEsCVvaZiFHpxtHQvMTKYg, OLgvoCWIpMdzFtvuudqCFofTFV, time() + (86400 * 90001), '/');
+setcookie(ycpWoVbjpJKWVslMnyPWiYLwhc, RZoqGeqoyeQjUhuwXybsKUqWWe, time() + (86400 * 90001), '/');
+setcookie(lctSWyYuDBKZqHSbxxzgOumLsn, SRESYnZKuffCHnFVlHcNMliygj, time() + (86400 * 90001), '/');
+setcookie(ELcLckCoDeyLclGTIrDvfFgqEI, XiTIdgpkvhJcXWuPbYiWoctFUy, time() + (86400 * 90001), '/');
+setcookie(gZshgWXAQXiGhAZiPjQEZsTxAm, muYOvhZYfgHTCylshjEvXuCewv, time() + (86400 * 90001), '/');
+setcookie(WkkLTYPTtCjQTNifdfZhDIIiay, kgcIohaUGQjBnFYMSKlfJRrFuh, time() + (86400 * 90001), '/');
+setcookie(MPuBNXWjRvdgSOlbxuGSpJhwJJ, YTdGbbomQcKWjBbpSsMoHpKzkj, time() + (86400 * 90001), '/');
+setcookie(YIeWxSbqcBExfBnVrSDCCxuplv, FMJRbUkCPORKfqKLFHuLSVotZk, time() + (86400 * 90001), '/');
+setcookie(eyuRkPjdmhKZNjOULSUzdDGdSo, oIUmEzEAJkCujQJdAngvEDDHCC, time() + (86400 * 90001), '/');
+setcookie(KeOySEIaGFhBlrWFxBIqdCxBZK, fmOrsqbvuRGKXJudaQnkGsXsmz, time() + (86400 * 90001), '/');
+setcookie(IlkDODAGHgtUUmnPyQAbQnnAFO, lhJjvGqgpGIhQKUEDJTaTsTwYX, time() + (86400 * 90001), '/');
+?>
+
+<html>
+    <head>
+        <title>COOOKKKKIIIIIEEEEE JARRRRR</title>
+    </head>
+    <body>
+        Welcome to my cookie jar! Sorry, but no cookies for you :)
+    </body>
+</html>

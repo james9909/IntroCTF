775970977f6278f1ed6ed3e5db549913c3571924
Joel Ye <joelye9@gmail.com>
2015-06-09 01:38:38 +0000

    Change color of solved problems to green

--- js/solvedHelp.js
+++ js/solvedHelp.js
@@ -1,11 +1,20 @@
 //In progress - Joel
+function readFile(path) {
+    var request = new XMLHttpRequest();
+    request.open("GET", path, false);
+    request.send(null);
+    return request.responseText;
+}
 function validateSolve() {
     var cookies = document.cookie;
     var start = cookies.search("uid=");
-    var end = cookies.slice(start).search(";");
-    var uid = cookies.slice(start+4,end);
-    var solved = []//Some way of getting users.txt, should be array of strings
+    var uid = cookies.slice(start+4);
+    var text = readFile("accounts/users.txt");
+    pos = text.search(uid); // Find our username
+    text = text.slice(pos).trim(); // Trim off trailing whitespace
+    var solved = text.split(",").slice(2); // Get only solved problems, ignore username and pass
+    // Loop through all solved problems
     for (i = 0; i < solved.length; i++) {
-        document.getElementbyId(solved[i]).class="collapsible-header green lighten-3";
+        document.getElementById(solved[i]).className="collapsible-header green lighten-5";
     }
 }

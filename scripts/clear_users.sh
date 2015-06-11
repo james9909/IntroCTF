fd87dd48603d8b203942d52adee35caa648123f4
James Wang <jameswang9909@hotmail.com>
2015-06-11 02:51:37 +0000

    Update clear_users.sh to clear teams.txt

--- scripts/clear_users.sh
+++ scripts/clear_users.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Clearing users.txt, login_list.txt, and scores.txt"
-echo "" > ../accounts/users.txt
+echo "" > ../accounts/teams.txt
 echo "" > ../accounts/scores.txt
 echo "" > ../accounts/login_list.txt
 echo "Done!"

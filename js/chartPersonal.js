68da3cf91d6e00ef38eecf36f4c43a2e3534a71b
James Wang <jameswang9909@hotmail.com>
2015-06-11 22:27:30 +0000

    Update pages

--- js/chartPersonal.js
+++ js/chartPersonal.js
@@ -1,9 +1,18 @@
 function makeChart()
-    {new Chartist.Line('.ct-chart', {
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-  series: [
-    [2, 3, 2, 4,4],
-    [0, 2.5, 3, 2, 3],
-    [1, 2, 2.5, 3.5, 4]
-  ]
-});}
+    {console.log("Hello")
+        new Chartist.Line('.ct-chart', {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+        series: [
+            {name:"series-1",
+            data:[2, 3, 2, 4,4]}
+            ]
+        },
+        {
+            series:{
+                'series-1': {
+                    lineSmooth: Chartist.Interpolation.step({postpone:false}),
+                    showArea:true
+                },
+        }}
+  
+  );}
---
+++ scripts/stay_awake.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while [ true ]		
+do		
+    ping www.facebook.com -c 2
+    clear
+    ls
+    sleep 1800
+done

8fd2ef1570f78aaf396ee43b0eb829fa855f008c
James Wang <jameswang9909@hotmail.com>
2015-06-14 20:20:17 +0000

    Minor aesthetical change to login form

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,9 @@ YELLOW="\033[1;33m"
 RESET="\033[m"
 
 printf "${YELLOW}Setting up users....${RESET}\n"
-cd ../accounts
+cd ..
+mkdir accounts
+cd accounts
 touch login_list.txt teams.txt solved.txt scores.txt users.txt
 chmod +707 *.txt
 printf "${GREEN}Success!${RESET}"

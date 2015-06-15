1125cc8456ba79a470c1d5dc98c3b9914102b8ab
James Wang <jameswang9909@hotmail.com>
2015-06-14 22:58:45 -0400

    Add readme to index.html

--- js/init.js
+++ js/init.js
@@ -8,4 +8,13 @@ $( document ).ready(function() {
     if ($('nav').length) {
         $('.toc-wrapper').pushpin({ top: $('nav').height() });
     }
+    $('.modal-trigger').leanModal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+
+    }
+    );
+
 });

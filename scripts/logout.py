0509f1b1aae33db8eb46a906849c10548cf6378f
James Wang <jameswang9909@hotmail.com>
2015-06-15 08:39:00 -0400

    Fixxxx

--- scripts/logout.py
+++ scripts/logout.py
@@ -17,6 +17,7 @@ def removeFromLogged(uid):
     while "\n" in logged_in:
         logged_in.remove("\n")
     for team in logged_in:
+        print hashlib.sha1(team.strip() + "salt").hexdigest()
         if hashlib.sha1(team.strip() + "salt").hexdigest() == uid:
             logged_in.remove(team)
     open("../accounts/login_list.txt", "w").write("".join(logged_in) + "\n")

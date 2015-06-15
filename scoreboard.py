4f97d5c40545b90581bf231bd4c1324c1e96b7cf
James Wang <jameswang9909@hotmail.com>
2015-06-15 13:05:20 -0400

    Fix none team

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import os, operator
+import os, operator, cgitb
 print "Content-Type: text/html\n"
 print ""
 
@@ -42,7 +42,7 @@ def gen_scoreboard(team_data, ranked, team):
     if len(team_data) == 0:
         print '<h5 class = "center">There are no teams!<h5>'
         return
-    if team != "":
+    if team != "" and team != None:
         print "<center>Team %s, with a rank of %d, has %d points" %(team, get_rank(ranked, team), team_data[team])
     print "<br>"
     print "<div class='container'>"
